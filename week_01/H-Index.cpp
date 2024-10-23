1c3a7ef55882b6e206518fc63b687d5b01eb2877
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-10-23 23:52:00 +0600

    sort & simple calculation

---
+++ week_01/H-Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        
+        int mxHIdx = 0, len = citations.size();
+        int mxTillNow;
+
+        for (int i = 0; i < len; ++i) {
+            mxTillNow = min(citations[i], len - i);
+            mxHIdx = max(mxHIdx, mxTillNow);
+        }
+
+        return mxHIdx;
+    }
+};
