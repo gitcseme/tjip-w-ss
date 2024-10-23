244c8e3b53923982b8681ac201d19a9436cb39f8
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-10-23 22:36:41 +0600

    xor of same number makes 0, (a^b)^c = a^(b^c)

---
+++ week_01/Single_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor_combined = 0, len = nums.size();
+
+        for (int i = 0; i < len; ++i) {
+            xor_combined = xor_combined ^ nums[i];
+        }
+
+        return xor_combined;
+    }
+};
