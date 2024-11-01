6c2388d88badd446901e77fcc33a7d9a1972b87d
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-11-01 07:24:19 +0600

    priority q to get the first ending meeting now

---
+++ week_01/Meeting_room_||.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a.end < b.end;
+        });
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (auto& interval : intervals) {
+            if (pq.empty()) {
+                pq.push(interval.end);
+                continue;
+            }
+
+            if (interval.start >= pq.top()) {
+                pq.pop();
+            }
+
+            pq.push(interval.end);
+        }
+
+        return pq.size();
+    }
+};
