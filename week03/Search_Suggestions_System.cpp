ff0c022937e719a8cb3f37751e47826939e1cf4d
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-11-22 21:39:38 +0600

    suggesting using trie & dfs

---
+++ week03/Search_Suggestions_System.cpp
@@ -0,0 +1,69 @@
+class Solution {
+private:
+    class TrieNode {
+    public:
+        bool end;
+        unordered_map<char, TrieNode*> next;
+    };
+
+    class Trie {
+    public:
+        TrieNode* root;
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(string word) {
+            auto curr = root;
+            for (char &ch : word) {
+                if (curr -> next[ch] == nullptr) {
+                    curr -> next[ch] = new TrieNode();
+                }
+                curr = curr -> next[ch];
+            }
+            curr -> end = true;
+        }
+    };
+
+    Trie* build(vector<string> &strs) {
+        auto trie = new Trie();
+        for (auto &str : strs) {
+            trie -> insert(str);
+        }
+        return trie;
+    }
+
+    void dfs(TrieNode *node, string &inputStr, vector<string> &results) {
+        if (node == nullptr || results.size() == 3) return;
+        if (node -> end) {
+            results.push_back(inputStr);
+        }
+
+        for (char ch = 'a'; ch <= 'z'; ++ch) {
+            if (node -> next.count(ch)) {
+                inputStr.push_back(ch);
+                dfs(node -> next[ch], inputStr, results);
+                inputStr.pop_back();
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        vector<vector<string>> suggestions;
+        auto trie = build(products);
+        auto rootPtr = trie -> root;
+
+        string typedWord = "";
+        for (auto ch : searchWord) {
+            typedWord.push_back(ch);
+            rootPtr = (rootPtr) ? rootPtr -> next[ch] : nullptr;
+
+            vector<string> results;
+            dfs(rootPtr, typedWord, results);
+            suggestions.push_back(results);        
+        }
+
+        return suggestions;
+    }
+};
