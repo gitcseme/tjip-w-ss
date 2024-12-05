c28e535fa67314c517f77370334b739de2f2c265
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-12-05 20:53:37 +0600

    calculate sum at each root & propagate the max chain above

---
+++ week_04/Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,28 @@
+// TC: O(N) where N is the total number of nodes.
+// MC: O(N) recursive stack space
+
+class Solution {
+private:
+    int mxSum = INT_MIN;
+    int findMaxPath(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int mxLeft =  findMaxPath(root -> left);
+        int mxRight = findMaxPath(root -> right);
+        
+        // calculate max path rooted at current node.
+        int mxAtRoot = root -> val + mxLeft + mxRight;
+        mxSum = max(mxSum, mxAtRoot);
+
+        // propagate max chain upward
+        int maxFromSubtrees = max(mxLeft, mxRight);
+        int mxChainSum = max({root -> val, (root -> val + maxFromSubtrees), 0});
+        return mxChainSum;
+    }
+
+public:
+    int maxPathSum(TreeNode* root) {
+        findMaxPath(root);
+        return mxSum;
+    }
+};
