021439cb1f81c564df22fb6461751aa584415075
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-12-25 15:08:34 +0600

    try build BST from preorder

---
+++ week05/Verify_Preorder_Sequence_in_BST.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    bool tryBuildBST(vector<int>& preorder, int& index, int low, int high) {
+        if (index == preorder.size()) return true;
+
+        int node = preorder[index];
+        if (node <= low || node >= high) return false;
+
+        index++;
+        auto trySetLeft  = tryBuildBST(preorder, index, low,  node);
+        auto trySetRight = tryBuildBST(preorder, index, node, high);
+
+        return trySetLeft || trySetRight;
+    }
+
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        int globalIndex = 0;
+        return tryBuildBST(preorder, globalIndex, INT_MIN, INT_MAX);
+    }
+};
