6edaf42209e179b579eddfab670bbbb0b500ba77
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2025-02-02 18:02:20 +0600

    binary search to find first occurance

---
+++ week_08/First_Bad_Version.cpp
@@ -0,0 +1,21 @@
+// TC: log(N) where N is number of versions
+// MC: O(1)
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int L = 1, R = n, M;
+
+        while (L < R) {
+            M = L + (R - L) / 2;
+            if (isBadVersion(M)) {
+                R = M;
+            }
+            else {
+                L = M + 1;
+            }
+        }
+
+        return L;
+    }
+};
