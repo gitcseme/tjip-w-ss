754dcd3c7f370022c6ee386de36d0f45da67db14
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2025-09-03 09:33:53 +0600

    chore: add time & space complexity

--- sliding_window/Longest_Substring_Without_Repeating_Characters.cpp
+++ sliding_window/Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,13 +1,17 @@
+/* TC: O(n)
+ * SC: O(1)
+*/
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int L = 0, R = 0, res = 0;
-        vector<int> f(256, 0);
+        vector<int> freq(256, 0);
 
         while (R < s.size()) {
-            f[s[R]]++;
-            while (f[s[R]] > 1) {
-                f[s[L]]--;
+            freq[s[R]]++;
+            while (freq[s[R]] > 1) {
+                freq[s[L]]--;
                 L++;
             }
             res = max(res, R - L + 1);
